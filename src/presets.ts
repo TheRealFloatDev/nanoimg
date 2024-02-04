0efbb801268900751d5661223459e202323957a5
Alexander <alexander.neitzel@garmingo.com>
2024-02-04 18:53:15 +0100

    Add presets for very high and extreme configurations

---
+++ src/presets.ts
@@ -0,0 +1,31 @@
+/*
+ *   Copyright (c) 2024 Garmingo
+ *   All rights reserved.
+ *   Unauthorized use, reproduction, and distribution of this source code is strictly prohibited.
+ */
+import { Configuration } from ".";
+
+
+export const VERY_HIGH: Configuration = {
+    enableColorQuantization: false,
+    colorTolerance: 25,
+    enableAlphaStripping: true,
+    enableAdaptiveFiltering: true,
+    floidSteinbergDitheringLevel: 0,
+    enableColorLimit: true,
+    colorLimit: 128,
+    enableQualityReduction: true,
+    quality: 60
+}
+
+export const EXTREME: Configuration = {
+    enableColorQuantization: false,
+    colorTolerance: 25,
+    enableAlphaStripping: true,
+    enableAdaptiveFiltering: false,
+    floidSteinbergDitheringLevel: 0,
+    enableColorLimit: true,
+    colorLimit: 16,
+    enableQualityReduction: true,
+    quality: 1
+}
