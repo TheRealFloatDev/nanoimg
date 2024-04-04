c81d594cd99b00bfd072a49416060f6f485a10e0
Alexander <alexander.neitzel@garmingo.com>
2024-04-04 18:49:43 +0200

    Update chroma subsampling options in add.test.ts

--- tests/add.test.ts
+++ tests/add.test.ts
@@ -39,31 +39,23 @@ test("Chroma subsampling", async () => {
     inputBuffer: await readFile(inputFile),
     outputFile,
     options: {
-      ...defaultConfig,
-      enableChromaSubsampling: false,
+      enableColorQuantization: false,
+      enableChromaSubsampling: true,
+      colorTolerance: 0,
+      enableAlphaStripping: false,
+      enableAdaptiveFiltering: false,
+      floidSteinbergDitheringLevel: 0,
+      enableColorLimit: false,
+      colorLimit: 256,
+      enableQualityReduction: false,
+      quality: 100,
     },
   });
 
   // Log the file sizes and the difference
   const inputStats = await stat(inputFile).then((stats) => stats.size);
   const outputStats = await stat(outputFile).then((stats) => stats.size);
-  console.log(`Input file size: ${inputStats} bytes`);
-  console.log(`Output file size (default): ${outputStats} bytes`);
+  console.log(`Output file size (chroma subsampling): ${outputStats} bytes`);
   console.log(`File size difference: ${inputStats - outputStats} bytes or ${(((inputStats - outputStats) / inputStats) * 100).toFixed(2)}%`);
-
-  await nano({
-    inputBuffer: await readFile(inputFile),
-    outputFile,
-    options: {
-      ...defaultConfig,
-      enableChromaSubsampling: true,
-    },
-  });
-
-  // Log the file sizes and the difference
-  const outputStats2 = await stat(outputFile).then((stats) => stats.size);
-  console.log(`Output file size (chroma subsampling): ${outputStats2} bytes`);
-  console.log(`File size difference to non-chroma: ${outputStats - outputStats2} bytes or ${(((outputStats - outputStats2) / outputStats) * 100).toFixed(2)}%`);
-  console.log(`File size difference to input: ${inputStats - outputStats2} bytes or ${(((inputStats - outputStats2) / inputStats) * 100).toFixed(2)}%`);
   expect(1).toBe(1);
 }, 35 * 1000); // 35 seconds
